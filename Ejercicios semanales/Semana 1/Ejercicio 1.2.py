66ca489c70b6670e57e158f74fe86a46e2e93c57
ArielBeaz <ariel.beaz@usach.cl>
2024-06-26 16:21:40 -0400

    ok

--- Ejercicios semanales/Semana 1/Ejercicio 1.2.py
+++ Ejercicios semanales/Semana 1/Ejercicio 1.2.py
@@ -1,10 +1,18 @@
 import requests
 import json
 
-url = 'tps://data.messari.io/api/v1/assets/bitcoin/metrics'
+url = 'https://data.messari.io/api/v1/assets/bitcoin/metrics'
 response = requests.get(url)
-
 response_json = response.json()
 
+fecha = response_json['status']['timestamp']
+volumen = response_json['data']['market_data']['volume_last_24_hours']
+open = response_json['data']['market_data']['ohlcv_last_1_hour']['open']
+high = response_json['data']['market_data']['ohlcv_last_1_hour']['high']
+low = response_json['data']['market_data']['ohlcv_last_1_hour']['low']
+close = response_json['data']['market_data']['ohlcv_last_1_hour']['close']
 
-print(response_json)
+bitcoin = {}
+bitcoin[fecha] = {'fecha' :fecha , 'volumen' : volumen ,'open' : open , 'high': high , 'low': low , 'close': close}
+
+print(bitcoin)
