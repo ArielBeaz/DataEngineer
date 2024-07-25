eeba6c801255ca79cd8edbaaf6c20db75f90de78
ArielBeaz <ariel.beaz@usach.cl>
2024-07-25 13:34:34 -0400

    ok

--- Ejercicios semanales/Semana 6/pytrends_.py
+++ Ejercicios semanales/Semana 6/pytrends_.py
@@ -1,9 +1,10 @@
 import pandas as pd
+import matplotlib as mp
 from pytrends.request import TrendReq
 pytrends = TrendReq()
 keywords = ["Fin del mundo", "Calentamiento global", "Terremoto",'Tsunami']
 pytrends.build_payload(keywords, cat=0, geo='', gprop='') # Datos de los ultimos 5 años
 stop_queries = pytrends.interest_over_time()[keywords]
-stop_queries.head()
+print(stop_queries.head())
 
-stop_queries.plot(kind='line',figsize=(12,6), xlabel='Fecha',ylabel='Interés de audiencia',title='Interés en el tiempo')
+print(stop_queries.plot(kind='line',figsize=(12,6), xlabel='Fecha',ylabel='Interés de audiencia',title='Interés en el tiempo'))
