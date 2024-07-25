d68ca49528b1ea5e9ebea70b703ebb3964c06d0b
ArielBeaz <ariel.beaz@usach.cl>
2024-07-25 13:08:15 -0400

    ok

--- Ejercicios semanales/Semana 6/pytrends.py
+++ Ejercicios semanales/Semana 6/pytrends.py
@@ -1,3 +1,9 @@
+import pandas as pd
 from pytrends.request import TrendReq
 
-pytrends = TrendReq(hl='en-US', tz=360)
+
+pytrends = TrendReq()
+keywords = ["Fin del mundo", "Calentamiento global", "Terremoto",'Tsunami']
+pytrends.build_payload(keywords, cat=0, geo='', gprop='') # Datos de los ultimos 5 años
+stop_queries = pytrends.interest_over_time()[keywords]
+stop_queries.head()
